c821116cb2941119fb670c4f5b0293bc9170e925
Ekaterina-SMAG <kijtos@mail.ru>
2024-09-13 18:54:44 +0300

    Вносит правки

---
+++ script.js
@@ -0,0 +1,14 @@
+const nav = document.querySelector('.nav');
+const navToggle = document.querySelector('.nav__toggle');
+
+nav.classList.remove('nav--nojs');
+
+navToggle.addEventListener('click', () => {
+  if (nav.classList.contains('nav--closed')) {
+    nav.classList.remove('nav--closed');
+    nav.classList.add('nav--opened');
+  } else {
+    nav.classList.add('nav--closed');
+    nav.classList.remove('nav--opened');
+  }
+});
