d1a03cbf6444ff7fe9dced758fa116ba9225f218
Ekaterina-SMAG <kijtos@mail.ru>
2024-09-16 19:54:41 +0300

    Вновит исправления

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,14 @@
-/* в этот файл добавляет скрипты*/
+const nav = document.querySelector('.nav');
+const navToggle = document.querySelector('.nav__toggle');
+
+nav.classList.remove('nav--nojs');
+
+navToggle.addEventListener('click', () => {
+  if (nav.classList.contains('nav--closed')) {
+    nav.classList.remove('nav--closed');
+    nav.classList.add('nav--opened');
+  } else {
+    nav.classList.add('nav--closed');
+    nav.classList.remove('nav--opened');
+  }
+});/* в этот файл добавляет скрипты*/
